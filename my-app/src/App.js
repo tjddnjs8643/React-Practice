407e66f8cc70e29d869ad5220f7c938294389e72
Lee <tjddnjs7543@gmail.com>
2021-11-19 17:15:37 +0900

    Recap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <UncontrolledComponent/>
       <ControlledComponent/>
+      {/* Recap */}
       {/* <Condition/> */}
       {/* <List/> */}
       {/* <Event/> */}
